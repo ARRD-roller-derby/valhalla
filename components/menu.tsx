ab73de1e17af06865f88994f284b4eb46b256c59
Kazerlelutin <b@bouteiller.contact>
2023-06-24 01:42:26 +0200

    fix routes

--- components/menu.tsx
+++ components/menu.tsx
@@ -8,20 +8,22 @@ export function Menu() {
   const isMobile = useIsMobile()
   return (
     <div className="flex gap-1 flex-auto justify-around p-3 md:flex-col">
-      {routes.map((route) => {
-        const Icon = route.icon
-        return (
-          <Link key={route.name} href={route.path}>
-            <div
-              className="color-arrd flex gap-1 items-center"
-              data-active={router.pathname === route.path}
-            >
-              <Icon className="fill-arrd text-3xl md:text-base" />
-              {!isMobile && ' ' + route.name}
-            </div>
-          </Link>
-        )
-      })}
+      {routes
+        .filter((route) => route.inMenu)
+        .map((route) => {
+          const Icon = route.icon
+          return (
+            <Link key={route.name} href={route.path}>
+              <div
+                className="color-arrd flex gap-1 items-center"
+                data-active={router.pathname === route.path}
+              >
+                <Icon className="fill-arrd text-3xl md:text-base" />
+                {!isMobile && ' ' + route.name}
+              </div>
+            </Link>
+          )
+        })}
     </div>
   )
 }
