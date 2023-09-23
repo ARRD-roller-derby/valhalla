98cca65dd247832ece6f70b41e4c64fd50adde47
Kazerlelutin <b@bouteiller.contact>
2023-09-23 15:11:13 +0200

    add api point for midgard

---
+++ pages/api/midgard/events/this-month.ts
@@ -0,0 +1,70 @@
+// Bibliothèque externe
+import { NextApiRequest, NextApiResponse } from 'next'
+import { MongoDb } from '@/db'
+import { checkRoles } from '@/utils/check-roles'
+import { Event } from '@/models'
+process.env.TZ = 'Europe/Paris'
+
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+import timezone from 'dayjs/plugin/timezone'
+import duration from 'dayjs/plugin/duration'
+import isBetween from 'dayjs/plugin/isBetween'
+import fr from 'dayjs/locale/fr'
+
+dayjs.extend(relativeTime)
+dayjs.extend(localizedFormat)
+dayjs.extend(timezone)
+dayjs.extend(duration)
+dayjs.extend(isBetween)
+dayjs.locale(fr)
+dayjs.tz.guess()
+dayjs.tz.setDefault('Europe/Paris')
+
+import { midgardMiddleWare } from '@/utils/midgard-middleware'
+import { IUser } from '@/models'
+
+// Initialiser le fuseau horaire
+process.env.TZ = 'Europe/Paris'
+
+async function thisMonth(_req: NextApiRequest, res: NextApiResponse, user: IUser) {
+  const start = dayjs().toISOString()
+  const isMember = checkRoles(['membre'], user)
+  const isAdmin = checkRoles(['bureau', 'dev'], user)
+  const between = {
+    start: {
+      $gte: start,
+      $lte: dayjs().endOf('month').toISOString(),
+    },
+  }
+  const or = []
+
+  if (!isMember) {
+    or.push({
+      ...between,
+      visibility: 'public',
+    })
+  } else if (isMember && !isAdmin) {
+    or.push({
+      ...between,
+      visibility: {
+        $in: ['membre', 'public'],
+      },
+    })
+  }
+
+  if (isAdmin) {
+    or.push({
+      ...between,
+    })
+  }
+
+  await MongoDb()
+  const events = await Event.find({ $or: or })
+  return res.status(200).json({ events })
+}
+
+const helper = (request: NextApiRequest, response: NextApiResponse) => midgardMiddleWare(request, response, thisMonth)
+
+export default helper
---
+++ utils/midgard-middleware.ts
@@ -0,0 +1,17 @@
+import { User } from '@/models'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { MongoDb } from '@/db'
+
+export async function midgardMiddleWare(request: NextApiRequest, response: NextApiResponse, helper: Function) {
+  const { headers } = request
+  const provider_id = headers?.['provider_id']
+
+  if (!headers?.['authorization'] || headers['authorization'].replace('Bearer ', '') !== process.env.MIDGARD_TOKEN)
+    return response.status(401).json({ error: 'non autorisé' })
+
+  if (!provider_id) return response.status(401).json({ error: 'aucun id utilisateur' })
+
+  await MongoDb()
+  const user = await User.findOne({ providerAccountId: provider_id })
+  return helper(request, response, user)
+}
