ae40bb34977e728d960dd00e4a795753e54edc34
Kazerlelutin <b@bouteiller.contact>
2024-01-22 16:09:15 +0100

    change sort for "répertoire" page

--- pages/api/members/index.ts
+++ pages/api/members/index.ts
@@ -67,6 +67,13 @@ export default async function members(req: NextApiRequest, res: NextApiResponse)
     .filter((member: any) => member.roles.some((role: any) => role.name.toLowerCase() === 'membre'))
     .filter((member: any) => !member.roles.find((role: any) => role.name.toLowerCase() === 'midgard'))
     .sort((a: any, b: any) => a.username.localeCompare(b.username))
+    .sort((a: any, b: any) => {
+      const aHasBureau = a.roles.some((role: any) => role.name.toLowerCase() === 'bureau')
+      const bHasBureau = b.roles.some((role: any) => role.name.toLowerCase() === 'bureau')
+      if (aHasBureau && !bHasBureau) return -1
+      else if (!aHasBureau && bHasBureau) return 1
+      return 0
+    })
 
   return res.status(200).json({ members })
 }
