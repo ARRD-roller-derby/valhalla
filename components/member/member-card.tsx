4d850d4d3ac27aa123696e68bc6a39ff7084a3fd
Kazerlelutin <b@bouteiller.contact>
2024-02-19 00:51:45 +0100

    Ajout Derbyname et N° roster sur répertoire

--- components/member/member-card.tsx
+++ components/member/member-card.tsx
@@ -12,11 +12,18 @@ export function MemberCard() {
     <Card>
       <div className="grid grid-cols-[auto_1fr] items-center gap-3 ">
         {member.avatar && <img src={member.avatar} className="h-16 w-16 rounded-full" />}
-        <div className="grid h-full grid-rows-[auto_1fr] content-between gap-1">
+        <div className="grid h-full grid-rows-[auto_1fr] content-between gap-2">
           <Link href={`/repertoire/${member.id}`}>
             <div className="text-md uppercase text-arrd-primary">{member.username}</div>
           </Link>
           {member.type && <div className="text-sm text-arrd-highlight first-letter:uppercase">{member.type}</div>}
+
+          {member?.type?.match(/derby/i) && (member.options_derbyname || member.options_nroster) && (
+            <div className="rounded-2 flex items-center justify-between gap-1 border border-arrd-bgLight p-1 text-sm">
+              <div className="text-arrd-highlight">{member.options_derbyname}</div>
+              <div className="font-bold text-arrd-primary">{member.options_nroster}</div>
+            </div>
+          )}
           <div className="flex items-end">
             <MemberRoles />
           </div>
