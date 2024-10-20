d3f84d7f1be0a8f38c85cdbaf62b97708ae881ef
Kazerlelutin <b@bouteiller.contact>
2024-10-20 18:05:21 +0200

    ajout des infos des invités dans les participants

--- pages/api/events/[id]/participants.ts
+++ pages/api/events/[id]/participants.ts
@@ -1,8 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { MongoDb } from '@/db'
-import { Event } from '@/models'
+import { Event, User } from '@/models'
 import { getDiscordMember } from '@/services/get-discord-member'
 import { authMiddleWare } from '@/utils/auth-middleware'
+import { DISCORD_TOKEN } from '@/utils'
+import { REST } from '@discordjs/rest'
+import { Routes } from 'discord-api-types/v10'
+
 process.env.TZ = 'Europe/Paris'
 
 async function event_participants(req: NextApiRequest, res: NextApiResponse, user: any) {
@@ -12,15 +16,44 @@ async function event_participants(req: NextApiRequest, res: NextApiResponse, use
   const { members } = await getDiscordMember()
 
   if (!event) return res.status(404).send('Événement non trouvé')
-  return res.status(200).json({
-    participants: event.participants.map((par: any) => {
-      const m = members.find((member) => member.id === par.userId)
-      return {
+  const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN)
+
+  const participants = []
+
+  for (const par of event.participants) {
+    const m = members.find((member) => member.id === par.userId)
+    if (!m) {
+      const baseUser: any = await User.findById(par.userId)
+
+      const providerAccountId = baseUser?.providerAccountId || baseUser._doc?.image.split('/')[4]
+
+      if (/^\d+$/.test(providerAccountId)) {
+        try {
+          const guest: any = await rest.get(Routes.user(providerAccountId))
+          participants.push({
+            ...guest,
+            ...par._doc,
+            avatar: baseUser._doc?.image,
+            name: guest?.nick || guest?.global_name || guest?.name,
+          })
+        } catch (e) {
+          participants.push({
+            ...par._doc,
+            name: baseUser.name,
+          })
+        }
+      }
+    } else {
+      participants.push({
         ...par._doc,
         ...m,
         name: m?.nick || m?.global_name || m?.name,
-      }
-    }),
+      })
+    }
+  }
+
+  return res.status(200).json({
+    participants,
   })
 }
 
