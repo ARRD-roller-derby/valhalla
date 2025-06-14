7eaec7d2a4011f0b24a3e5c991de6395d0805a35
Kazerlelutin <b@bouteiller.contact>
2025-06-14 21:14:45 +0200

    fix pusher discord

--- services/publish-to-discord.ts
+++ services/publish-to-discord.ts
@@ -1,7 +1,7 @@
 // Bibliothèque interne
 import { DISCORD_LINKS } from '@/utils'
 
-export async function publishToDiscord(type: 'event' | 'news' | 'logs', content: string, thread_name: string) {
+export async function publishToDiscord(type: 'event' | 'news' | 'logs', content: string) {
   const webhookUrl = DISCORD_LINKS[type]
   if (!content || !webhookUrl) return
 
@@ -15,12 +15,10 @@ export async function publishToDiscord(type: 'event' | 'news' | 'logs', content:
       body: JSON.stringify({ content }),
     })
 
-    console.log('=============>', response)
-
     if (!response.ok) {
       console.error("Erreur lors de l'envoi du message:", await response.text())
     } else {
-      console.log('Message envoyé avec succès')
+      console.log('Message envoyé avec succès', response.status)
     }
   } catch (error) {
     console.error('Erreur lors de la publication sur Discord:', error)
